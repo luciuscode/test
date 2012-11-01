b55252e008b01c9c1ed6b5c22fe67ca291455175
lucius <lucius@192.168.1.15>
2012-11-01 11:27:07 +0100

    1

--- org.emftext.language.forms/metamodel/forms.cs
+++ org.emftext.language.forms/metamodel/forms.cs
@@ -2,7 +2,23 @@ SYNTAXDEF forms
 FOR <http://www.emftext.org/language/forms>
 START EntityModel
 
+IMPORTS {
+	org.eclipse.emf.ecore:<http://www.eclipse.org/emf/2002/Ecore>
+}
 
+
+OPTIONS {
+	reloadGeneratorModel = "true";
+	generateCodeFromGeneratorModel = "true";
+	disableLaunchSupport = "true";
+	disableNewProjectWizard = "true";	
+	srcFolder = "src/main/java";
+	srcGenFolder = "src/gen/java";
+	
+	uiSrcFolder = "src/main/java";
+	uiSrcGenFolder = "src/gen/java";
+	}
+	
 RULES {
 	EntityModel ::= "model" types*;
 	Entity ::= abstract["abstract" : ""] "entity" name[] "{" features* "}";
